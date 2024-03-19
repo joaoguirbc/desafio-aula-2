ad84fec6109489c796eb076337a6d2a5007bec98
João Guilherme Cavalcanti <joaogui76@gmail.com>
2024-03-19 11:47:47 -0300

    alteração do print e remoção hello world

--- teste.py
+++ teste.py
@@ -1,7 +1,5 @@
-print('Hello, world')
-
 time_usuario = input('Pra qual time você torce?')
 if time_usuario == 'Sport':
   print('Maior do Nordeste!')
 else:
-  print('Meus pêsames')
+  print('Meus pêsames, você não sabe o que é ser feliz.')
